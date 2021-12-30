7cabfd98a51d522adef07f2d7f711762dfd39219
Guilherme Reis <grrbm2@gmail.com>
2021-12-30 14:35:30 -0300

    fixed icons on android

---
+++ src/Containers/MainContainer.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from 'react'
+import {
+  View,
+  ActivityIndicator,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
+} from 'react-native'
+import { useDispatch } from 'react-redux'
+import { useTranslation } from 'react-i18next'
+import { Brand } from '@/Components'
+import { useTheme } from '@/Hooks'
+import { useLazyFetchOneQuery } from '@/Services/modules/users'
+import { changeTheme } from '@/Store/Theme'
+
+const MainContainer = () => {
+  const { t } = useTranslation()
+  const { Common, Fonts, Gutters, Layout } = useTheme()
+  const dispatch = useDispatch()
+
+  const [userId, setUserId] = useState('9')
+  const [fetchOne, { data, isSuccess, isLoading, isFetching, error }] =
+    useLazyFetchOneQuery()
+
+  useEffect(() => {
+    fetchOne(userId)
+  }, [fetchOne, userId])
+
+  return (
+    <ScrollView
+      style={Layout.fill}
+      contentContainerStyle={[
+        Layout.fill,
+        Layout.colCenter,
+        Gutters.smallHPadding,
+      ]}
+    >
+      <View
+        style={[
+          Layout.row,
+          Layout.rowHCenter,
+          Gutters.smallHPadding,
+          Gutters.largeVMargin,
+          Common.backgroundPrimary,
+        ]}
+      >
+        <Text style={[Layout.fill, Fonts.textCenter, Fonts.textSmall]}>
+          {t('example.labels.userId')}
+        </Text>
+        <TextInput
+          onChangeText={setUserId}
+          editable={!isLoading}
+          keyboardType={'number-pad'}
+          maxLength={1}
+          value={userId}
+          selectTextOnFocus
+          style={[Layout.fill, Common.textInput]}
+        />
+      </View>
+    </ScrollView>
+  )
+}
+
+export default MainContainer
--- src/Navigators/Main.js
+++ src/Navigators/Main.js
@@ -1,14 +1,36 @@
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import { ExampleContainer } from '@/Containers'
+import { MainContainer } from '@/Containers'
+import Icon from 'react-native-vector-icons/FontAwesome'
 
 const Tab = createBottomTabNavigator()
 
 // @refresh reset
 const MainNavigator = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={ExampleContainer} />
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName
+
+          if (route.name === 'Home') {
+            iconName = focused
+              ? 'ios-information-circle'
+              : 'ios-information-circle-outline'
+          } else if (route.name === 'Settings') {
+            iconName = focused ? 'ios-list-box' : 'ios-list'
+          }
+
+          // You can return any component that you like here!
+          return <Icon name="rocket" size={30} color="#900" />
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: 'tomato',
+        inactiveTintColor: 'gray',
+      }}
+    >
+      <Tab.Screen name="Home" component={MainContainer} />
     </Tab.Navigator>
   )
 }
